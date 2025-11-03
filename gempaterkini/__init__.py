4c0e40c29754938e7012bd836cda134cc4bcf6a0
Dhimas Primajaya <131242124+dhimasprimaj@users.noreply.github.com>
2025-11-03 13:23:12 +0700

    Finish real data from BMKG.go.id

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -50,9 +50,9 @@ def ekstraksi_data():
             koordinat = semua_nilai_span[2].text.strip()
         else:
             # Jika list kosong (0 elemen)
-            magnitudo = "N/A"
-            kedalaman = "N/A"
-            koordinat = "N/A"
+            magnitudo = None
+            kedalaman = None
+            koordinat = None
 
         hasil["tanggal"] = tanggal
         hasil["waktu"] = waktu
