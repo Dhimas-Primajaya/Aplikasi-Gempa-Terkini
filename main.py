5fb8658bb92210e0851884bee1f62b4aa90d16f1
Dhimas Primajaya <131242124+dhimasprimaj@users.noreply.github.com>
2025-11-03 13:57:27 +0700

    Update package name

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ Aplikasi Gempa Terkini
 MODULARISASI DENGAN FUNCTION
 """
 
-#Cara pertama (kurang recommend)
-
-# from gempaterkini import ekstraksi_data, tampilkan_data
-#
-# if __name__ == '__main__':
-#     print("Aplikasi Utama:")
-#     result = ekstraksi_data()
-#     tampilkan_data(result)
-
-#Cara Kedua (recommended)
-
-import gempaterkini
+import gempaterupdate
 
 if __name__ == "__main__":
     print("Aplikasi Gempa Terkini")
-    result = gempaterkini.ekstraksi_data()
-    gempaterkini.tampilkan_data(result)
+    result = gempaterupdate.ekstraksi_data()
+    gempaterupdate.tampilkan_data(result)
